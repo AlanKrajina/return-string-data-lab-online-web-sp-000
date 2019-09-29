86995206e39ba135b1b98f8cf6eb7194cd4b2836
Alan Krajina <alankrajina88@gmail.com>
2019-09-29 11:50:25 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
 def create
-  @product = Product.create(post_params)
+  @product = Product.create(params[:product_attributes])
   @product.save
   redirect_to product_path(@product)
 end
