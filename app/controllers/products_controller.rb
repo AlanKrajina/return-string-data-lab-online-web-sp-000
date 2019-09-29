fc7cddac8fc289685f0c1763a82ff9a35521b4fa
Alan Krajina <alankrajina88@gmail.com>
2019-09-29 12:02:05 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,15 @@
 class ProductsController < ApplicationController
 
-def create
-  @product = Product.create(params[:product_attributes])
-  @product.save
-  redirect_to products_path
-end
+  def create
+    @product = Product.create(product_params)
+    redirect_to products_path
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :inventory, :description)
+  end
+
 end
