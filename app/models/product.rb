6250886e5aeb09fb44a3f32bc45bb8ddd1cdfe70
Alan Krajina <alankrajina88@gmail.com>
2019-09-29 11:56:05 +0000

    Automatically backed up by Learn

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,8 @@ class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
 
-  def initialize(product_attributes)
-    @inventory = product_attributes["inventory"]
-  end
+#  def initialize(product_attributes)
+#    @inventory = product_attributes["inventory"]
+#  end
 
 end
